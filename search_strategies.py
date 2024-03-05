9047d26e0c2bb4cc8c218b7728ac34a7cf83c1a2
ddiissttrraaiinntt <148001224+ddiissttrraaiinntt@users.noreply.github.com>
2024-03-05 21:42:31 +0300

    Update search_strategies.py

--- search_strategies.py
+++ search_strategies.py
@@ -60,7 +60,7 @@ def dls(current_node: "Node", visited_states: set,
     :param depth_limit: Ограничение в глубину для поиска с ограничением.
     :return: Найденное конечное состояние и затраченное для этого количество итераций.
     """
-   iterations += 1  # Увеличиваем счетчик итераций
+    iterations += 1  # Увеличиваем счетчик итераций
 
     # Проверяем, достигнуто ли конечное состояние
     if check_final(current_node.current_state):
@@ -80,21 +80,14 @@ def dls(current_node: "Node", visited_states: set,
     if depth_limit is not None and current_node.depth >= depth_limit:
         if not dls_recursive_with_stack.limit_reached:
             print("\nДостигнуто ограничение глубины!")
-            dls_recursive_with_stack.limit_reached = True
+            stack.clear() # Очищаем очередь
+            dls_recursive_with_stack.limit_reached = True # Меняем флаг достижения глубины
         return None, iterations
 
     # Получаем новые состояния из текущего узла
     new_states_dict = get_new_states(current_node.current_state)
-
-    # Рекурсивно исследуем каждого потомка
-    for child_action, child_state in new_states_dict.items():
-        child_hash_value = state_hash(child_state)
-        if child_hash_value not in visited_states:
-            # Передаем действие в качестве атрибута при создании узла
-            child_node = Node(child_state, current_node, child_action, current_node.path_cost + 1, current_node.depth + 1)
-            stack.append(child_node)
-
-    # Отладочный вывод текущего узла и его потомков
+    
+    # Отладочный вывод текущего узла и всех его потомков по шагам
     if DEBUG:
         print(f"----------------Шаг: {iterations}.---------------- \n")
         print("Текущий узел:", end=' ')
@@ -102,14 +95,24 @@ def dls(current_node: "Node", visited_states: set,
             print("Корень дерева")
         print_node(current_node)
         print("Потомки:")
-        for child_action, child_state in new_states_dict.items():  # Итерируем по всем новым состояниям
+                
+    # Рекурсивно исследуем каждого потомка
+    for child_action, child_state in new_states_dict.items():
+        child_hash_value = state_hash(child_state) 
+        if DEBUG: # При отладке создаем каждого потомка
             child_node = Node(child_state, current_node, child_action, current_node.path_cost + 1, current_node.depth + 1)
-            if state_hash(child_state) in visited_states:
-                print_node(child_node, is_duplicate=True)
+        if child_hash_value not in visited_states:
+            if not DEBUG: # Иначе - только потомков с уникальными состояниями
+                child_node = Node(child_state, current_node, child_action, current_node.path_cost + 1, current_node.depth + 1)
             else:
-                print_node(child_node)
+                print_node(child_node) # Выводим потомков с уникальными состояниями при отладке
+            stack.append(child_node) # Помещаем узел в очередь
+        else:
+            if DEBUG:
+                print_node(child_node, is_duplicate=True) # Выводим потомков с повторными состояниями при отладке
+    if DEBUG:
         input("Нажмите 'Enter' для продолжения...")
-
+                
     # Обработка потомков
     while stack:
         next_node = stack.pop()
